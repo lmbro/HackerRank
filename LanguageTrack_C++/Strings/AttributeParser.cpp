6b243ad020082ff8f4c33e5c0d5ba09007369aae
Lucas Brown <lucasbrown1871@gmail.com>
2017-06-04 21:59:01 -0500

    Adding/removing tags working

--- LanguageTrack_C++/Strings/AttributeParser.cpp
+++ LanguageTrack_C++/Strings/AttributeParser.cpp
@@ -30,10 +30,15 @@ void readLineHDML(std::unordered_map<std::string,std::string> &hash){
     std::cout << source << std::endl;
     while(getline(sourcestream, temp, ' ')){
         if(temp[0]=='<'){ // Tag
-            if(temp[1]=='/'){ //Closing tag
+            temp = temp.substr(1,temp.length()-2);
+            if(temp[0]=='/'){ //Closing tag
                 std::cout << temp << " : Closing tag" << std::endl;
+                (tag==temp.substr(1)) ? (tag.erase()) : (tag = tag.substr(0,tag.length()-temp.length()));
+                std::cout << "Tag Name: " << tag << std::endl;
             }else{ // Opening tag
                 std::cout << temp << " : Opening tag" << std::endl;
+                (tag.empty()) ? (tag=temp) : (tag+='.'+temp);
+                std::cout << "Tag Name: " << tag << std::endl;
             }
         }else{  // Attribute assignment
             std::cout << temp << " : Attribute assignment" << std::endl;
