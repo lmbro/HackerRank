4b4f6296ec643d8eff85e1018a63e43280ddd7bc
Lucas Brown <lucasbrown1871@gmail.com>
2017-06-05 16:39:35 -0500

    LC++/Strings/AttributeParser a few cases passed, a few failed - backing up before fixing

--- LanguageTrack_C++/Strings/AttributeParser.cpp
+++ LanguageTrack_C++/Strings/AttributeParser.cpp
@@ -4,9 +4,12 @@
 #include <vector>
 #include <unordered_map>
 
-void readLineHDML(std::unordered_map<std::string,std::string> &hash);
+void readHDML(int n, std::unordered_map<std::string,std::string> &hash);
 
 int main(){
+    int n,q;
+    std::cin >> n >> q; 
+    std::cin.ignore();
 
     // Hashtable containg tag names and attributes
     // Key = <tagName>~<attribute>
@@ -14,44 +17,53 @@ int main(){
     std::unordered_map < std::string, std::string > hashtable;
 
     // Read HDML source
-    readLineHDML(hashtable);
+    readHDML(n, hashtable);
+
+    // Print HDML queries
+    for(int i=0; i<q; i++){
+        std::string query;
+        getline(std::cin,query);
+        if(hashtable[query].empty()){
+            std::cout << "Not Found!" << std::endl;
+        }else{
+            std::cout << hashtable[query] << std::endl;
+        }
+    }
+
     return 0;
 }
 
-void readLineHDML(std::unordered_map<std::string,std::string> &hash){
-    std::cout << "The Eagle has landed" << std::endl;
-
+void readHDML(int n, std::unordered_map<std::string,std::string> &hash){
     std::string source, temp;
     std::string tag, attribute, value;
-    bool att_val_flag = true; // True = attribute, false = value
+    
+    for(int i=0; i<n; i++){
+        bool att_val_flag = true; // True = attribute, false = value
 
-    getline(std::cin,source);
-    std::stringstream sourcestream(source);
+        getline(std::cin,source);
+        std::stringstream sourcestream(source);
 
-    //DEBUGstd::cout << source << std::endl;
-    while(getline(sourcestream, temp, ' ')){
-        if(temp[0]=='<'){ // Tag
-            temp = temp.substr(1,temp.length()-2);
-            if(temp[0]=='/'){ //Closing tag
-                //DEBUGstd::cout << temp << " : Closing tag" << std::endl;
-                (tag==temp.substr(1)) ? (tag.erase()) : (tag = tag.substr(0,tag.length()-temp.length()));
-                //DEBUGstd::cout << "Tag Name: " << tag << std::endl;
-            }else{ // Opening tag
-                //DEBUGstd::cout << temp << " : Opening tag" << std::endl;
-                (tag.empty()) ? (tag=temp) : (tag+='.'+temp);
-                //DEBUGstd::cout << "Tag Name: " << tag << std::endl;
-            }
-        }else{  // Attribute assignment
-            //DEBUGstd::cout << temp << " : Attribute assignment" << std::endl;
-            if(temp=="=") continue;
-            if(att_val_flag){
-                attribute=temp;
-                att_val_flag=false;
+        while(getline(sourcestream,temp,' ')){
+            if(temp[0]=='<'){ // Tag
+                temp = temp.substr(1); // Remove leading <
+                if(temp[0]=='/'){ // Closing tag
+                    (tag==temp.substr(1)) ? (tag.erase()) : (tag=tag.substr(0,tag.length()-temp.length())); // Removes temp from tag name
+                }else{
+                    (tag.empty()) ? (tag=temp) : (tag+='.'+temp);
+                }
             }else{
-                hash[tag+"~"+attribute]=temp;
-                att_val_flag=true;
-                std::cout << tag+"~"+attribute << " = " << hash[tag+"~"+attribute] << std::endl;
-            }         
+                if(temp=="=") continue; // Don't process =
+                if(att_val_flag){
+                    attribute=temp;
+                    att_val_flag=false;
+                }else{
+                    (temp[temp.length()-1]=='>') ? (temp=temp.substr(1,temp.length()-3)) : (temp=temp.substr(1,temp.length()-2)); // Remove leading/trailing characters
+                    hash[tag+"~"+attribute]=temp;
+                    att_val_flag=true;
+                }
+            }
         }
     }
 }
+
+
