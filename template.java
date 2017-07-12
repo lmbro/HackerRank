b2f0e3509443498b850123142c835eeaa2908388
Lucas Brown <lucasbrown1871@gmail.com>
2017-07-11 23:14:38 -0500

    Java End-Of-File solution, minor template updates (still playing with javadoc a little)

--- template.java
+++ template.java
@@ -3,8 +3,9 @@
 template.java
 Purpose: HackerRank Challenge, CHALLENGE_NAME
 
-Author: Lucas Brown
-Version: 1.0 0/0/2017
+@author  Lucas Brown
+@version X.X
+MONTH DAY, YEAR
 
 Some challenges have code provided that is necessary to complete the challenge. Any code provided by HackerRank and not written by Lucas Brown will be identified.
 
