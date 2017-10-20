8ddd3a381cc95112e3e187d642bb13319e9f89f7
Lucas Brown <lucasbrown1871@gmail.com>
2017-10-20 15:09:06 -0500

    swap_case.py

---
+++ LanguageTrack_Python/Strings/README.md
@@ -0,0 +1,16 @@
+# Description
+
+Python practice - working with Strings.
+
+We must move forward! I use Python 3.
+
+
+## helloword.py
+The man, the myth, the legend.
+
+Python's "main" function
+
+## swap_case.py
+
+string.swapcase() -- That's it, that's the entire problem.
+
--- LanguageTrack_Python/Strings/split_and_join.py
+++ LanguageTrack_Python/Strings/split_and_join.py
@@ -1,6 +1,6 @@
 #
-# File:    swap_case.py
-# Purpose: HackerRank Python Track, sWAP cASE
+# File:    split_and_join.py
+# Purpose: HackerRank Python Track, String Split and Join
 # Author:  Lucas Brown
 # Date:    October 20, 2017
 # Python:  3.5.3
@@ -8,47 +8,40 @@
 # Some challenges have code provided that is necessary to complete the challenge. Any code provided by HackerRank and not written by Lucas Brown will be identified.
 #
 #
+# Background
+# -----------
+# In Python, a string can be split ona  delimiter
+#
+#
 # Task
 # -----
-# You are given a string S. Your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
 #
 #
 # Input Format
 # -------------
-# A single line containing a string S.
 #
 #
 # Output Format
 # --------------
-# Print the modified string S.
 #
 #
 # Constraints
 # ------------
-# 0 < len(S) <= 1000
 #
 #
 # Sample Input
 # -------------
-# HackerRank.com presents "Pythonist 2".
 #
 #
 # Sample Output
 # --------------
-# hACKERrANK.COM PRESENTS "pYTHONIST 2".
 #
-
-def swap_case( s ):
-    return s.swapcase()
 
 #----------------------------------------------------------------
 #           BEGIN (1/1): Code provided by HackerRank
 #----------------------------------------------------------------
-if __name__ == "__main__":
 
-    s = input()
-    result = swap_case(s)
-    print( result )
+
 #----------------------------------------------------------------
 #           End (1/1): Code provided by HackerRank
 #----------------------------------------------------------------
